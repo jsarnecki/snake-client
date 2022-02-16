e8a5b65f5e42948d29216e82a831f1c17d876fd9
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-16 22:59:45 +0000

    Moved some files around, using ES6 simplified syntax

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require("net");
+// const net = require("net");
+const {connect} = require("./client");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log("You receive this message from server: ", data);
-  })
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting...");
 connect();
